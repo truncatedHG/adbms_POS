5f72c5126da96a68a5aaf4850322899e887f539e
truncatedHG <aldriandiaz123@gmail.com>
2026-05-08 15:35:32 +0800

    add spring boot

---
+++ pos/src/main/java/com/adbms/pos/PosApplication.java
@@ -0,0 +1,14 @@
+package com.adbms.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosApplication.class, args);
+	}
+
+}
